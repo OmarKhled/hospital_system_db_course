d15bc2d685dd0568710c99abef10ca11b0d18867
Omar Khaled <o.abuelfetohahmed@nu.edu.eg>
2024-01-03 18:49:56 +0200

    rectify medicines

--- app/routes/medicines.tsx
+++ app/routes/medicines.tsx
@@ -4,18 +4,18 @@ const Medicines = () => {
   return (
     <div className="prose lg:prose-xl w-full">
       <h1 className="mx-auto text-center pt-12 !mb-0 pb-0">
-       Medicines Management
+        Medicines Management
       </h1>
       <div className="grid grid-cols-2 mx-auto w-2/4 mt-5 gap-4">
         <Link
           className="bg-slate-200 w-full flex justify-center py-1 px-6 no-underline rounded-md border border-slate-300 mx-auto"
-          to={"/medicine/view"}
+          to={"/medicines/view"}
         >
           View
         </Link>
         <Link
           className="bg-slate-200 w-full flex justify-center py-1 px-6 no-underline rounded-md border border-slate-300 mx-auto"
-          to={"/medicine/add"}
+          to={"/medicines/add"}
         >
           Add
         </Link>
